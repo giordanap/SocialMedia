b8860a9b49d56871ab6101f4ca408f3a43915392
giordanap <40145046+giordanap@users.noreply.github.com>
2021-08-26 17:20:21 -0500

    inicio

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            // Devuelve un status 200
+            return Ok(null);
+        }
+    }
+}
